2db4afd31fc524cfdb6d6bd953da9efdd6f08fef
harrygcoppock <hgc19@ic.ac.uk>
2022-03-17 12:31:41 +0000

    tidy up

--- other_useful_scripts/define_own_torch_dataset.py
+++ other_useful_scripts/define_own_torch_dataset.py
@@ -4,27 +4,28 @@ please see https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 for more details
 '''
 import os
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
-from torch.datasets import make_dataset, find_classes
 from skimage import io
+import pandas as pd
 
 '''
 torch.utils.data.Dataset is an abstract class representing a dataset. Your custom dataset should inherit Dataset and override the following methods:
 
 __len__ so that len(dataset) returns the size of the dataset.
-__getitem__ to support the indexing such that dataset[i] can be used to get iith sample.
+__getitem__ to support the indexing such that dataset[i] can be used to get ith sample.
 
 In most cases before one can begin training an ML model on a dataset you first need to decide the train/dev/test splits. 
 This processs often takes up a considerable amount of time and it is important that the sets are balanced and bias in the
-datasets have been addressed. It is key that the test set must be engineered in a manner to act a a truly unseen dataset.
-On creation of the train/val/test splits it is common practive to 
-In the case of CW1 it is a slightly different situation that the training and validation sets have been provided to you as 
+datasets have been addressed. It is key that the test set is engineered in a manner to act as a truly unseen dataset.
+On creation of the train/val/test splits it is common practive to create a csv file detailing file paths + split designation. 
+In the case of CW1 it is a slightly different situation, that the training and validation sets have been provided to you as 
 as single dataset and it is down to you to create the validation set. By default this is just a random split. When one 
-wants to create a different transform for train and validation however one runs into complications. Best practice would be
-to create a csv/txt detailing each sample's path and whether it is part of train/val/test. Then create a custom dataset with
-varying transforms/preprocessing.
+wants to create a different transform for train and validation however one runs into complications as it is 
+the dataset class which handles transforms in the torch api. Best practice would be to create a csv/txt detailing each
+sample's path and whether it is part of train/val/test. Then create a custom dataset with varying transforms/preprocessing.
 
 Below is an example of how you might do this.
 '''
@@ -33,7 +34,7 @@ class CustomDataset(Dataset):
     def __init__(self, csv_file, root_dir, transform=None, split='train'):
         """
         Args:
-            csv_file (string): Path to the csv file with annotations.
+            csv_file (string): Path to the csv file with meta data + splits
             root_dir (string): Directory with all the images.
             transform (callable, optional): Optional transform to be applied
                 on a sample.
@@ -41,13 +42,14 @@ class CustomDataset(Dataset):
         """
         self.split = split
         self.dataset = pd.read_csv(csv_file)
+        # so now only instances in split can be called.
         self.dataset = self.dataset[self.dataset['split'] == self.split]
         self.root_dir = root_dir
         self.transform = transform
 
     def __getitem_(self, idx):
         img_name = os.path.join(self.root_dir,
-                                self.dataset_paths['paths'].iloc[idx]) 
+                                self.dataset['paths'].iloc[idx]) 
         image = io.imread(img_name)
         label = self.dataset['label'].iloc[idx]
         if self.transform:
@@ -57,40 +59,38 @@ class CustomDataset(Dataset):
     def __len__(self):
         return len(self.dataset_path)
 
+'''
+We can also define custom transforms. For this we create new transforms as callable
+classes - they just need the __call__ method. You can also add an __init__ if you want
+to apply some additional set up processing
+'''
 
-def create_validation_set(path, n):
-    '''
-    Args:
-        path (str): path to the unzipped dataset
-        n (float): frac of set to make validation
-    Returns:
-        splits (csv): lists file paths with corresponding split, e.g. train/val
+class BareBonesTransform(object):
+    """
+    basic buidling block for a custom transform
+    p: place holder for an arg that you may want to pass to the contructor (not needed)
+    """
 
-        path              |    split
-        ----------------------------------
-        elephant/1234.png | train
-        tiger/1234.png    | validation
-        lion/1234.png     | train
-    '''
-    _, class_to_indx = find_classes(path)
-    instances = make_dataset(path, class_to_indx) # gen list of (path_to_instance, class)
-    # randomly sample n of the train set without replacement. Note this does not sample evenly from 
-    # test set.
-    df = pd.DataFrame(instances, columns=['path', 'split'])
-    val = df.sample(frac=n, random_state=42)
-    df['split'] = df[df in ]
-    return df
+    def __init__(self, p):
+        assert isinstance(p, float)
+        self.p = p
+
+    def __call__(self, x):
+
+        assert isinstance(x, torch.Tensor)
+        if random.uniform(0,1) < self.p:
+            # do something to x some of the time
+            x = x**2
+
+        return x
 
 
-def main(path):
+def main(df, path):
     '''
     path (str): path to dataset directory
     '''
-    df = create_validation_set
-    train_dataset = CustomDataset(df, path, split='train', transform=train_transform)
-    val_dataset = CustomDataset(df, path, split='val', transform=test_transform)
-    test_datset =CustomDataset(test_df, test_path, split='test', transform=test_transform)
+    train_dataset = CustomDataset(path_to_csv, path_to_datset, split='train', transform=train_transform)
+    val_dataset = CustomDataset(path_to_csv, path_to_dataset, split='val', transform=test_transform)
+    test_datset =CustomDataset(path_to_csv, path_to_datset, split='test', transform=test_transform)
 
 
-if __name__ == '__main__':
-    train_dataset = CustomDataset(split='train')
